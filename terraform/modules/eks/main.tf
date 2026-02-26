6a2256b2596ba2c9be99fa8d09bf793b84e96fd7
Fabricio Valdivia <walter.valdivia7685242@gmail.com>
2026-02-25 20:45:59 -0500

    proyecto06-10

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -27,3 +27,66 @@ resource "aws_eks_node_group" "nodes" {
 
   depends_on = [aws_eks_cluster.this]
 }
+
+# 1. Configurar el OIDC Provider (necesario para vincular K8s con IAM)
+# Obtiene el certificado TLS del cluster
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+# Crea el proveedor de identidad en IAM
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+# 2. Crear el Rol IAM y la Política de Confianza (Trust Policy)
+# Esta política permite que el ServiceAccount 'ebs-csi-controller-sa' asuma el rol
+data "aws_iam_policy_document" "ebs_csi_trust" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ebs_csi_role" {
+  name               = "${var.cluster_name}-ebs-csi-role"
+  assume_role_policy = data.aws_iam_policy_document.ebs_csi_trust.json
+}
+
+# Adjuntar la política oficial de AWS para EBS
+resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi_role.name
+}
+
+# 3. Instalar el Add-on en el Cluster EKS
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name             = aws_eks_cluster.this.name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi_role.arn
+  resolve_conflicts        = "OVERWRITE"
+
+  # Es buena práctica esperar a que los nodos existan
+  depends_on = [
+    aws_eks_node_group.nodes
+  ]
+}
